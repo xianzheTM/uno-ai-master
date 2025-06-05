711a2328d8bbfaef033aa8e77d136930d8a9ae49
yanglei <yanglei02@agora.io>
2025-06-05 14:20:55 +0800

    fix: 资源文件404

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // GitHub Pages部署时需要设置base路径
+  // GitHub Pages项目站点部署时需要设置base路径
   base: process.env.NODE_ENV === 'production' ? '/uno-ai-master/' : '/',
   resolve: {
     alias: {
